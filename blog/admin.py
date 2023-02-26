b07bc8c21ecfe318df51a9e0a53967be6c6173d4
dav_pr <dav_pr@tutanota.com>
2023-02-26 15:50:10 +0200

    ini  commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(Posts)
+admin.site.register(Comments)
+admin.site.register(LikeComments)
+admin.site.register(LikePost)
